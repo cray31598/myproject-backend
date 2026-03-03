bf4037d204bfb1961b7c5c7c3dfe1506b4386c5e
max-doppelt <superdev0420@gmail.com>
2026-03-03 09:56:15 -0500

    update

--- clear-db.js
+++ clear-db.js
@@ -2,9 +2,8 @@
  * Clear all data from the database while keeping table structure.
  * Usage: node clear-db.js
  */
-import db, { save, close } from './db.js';
+import db, { save } from './db.js';
 
 db.run('DELETE FROM invites');
 save();
-close();
 console.log('[clear-db] All rows deleted from invites. Schema unchanged.');
